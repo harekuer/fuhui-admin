52fd5293a44712322126f804e03f1792ce136388
xiaojunjunli <2127528604@qq.com>
2020-01-11 17:58:26 +0800

    调试用户名

--- src/models/login.js
+++ src/models/login.js
@@ -16,7 +16,7 @@ const Model = {
         type: 'changeLoginStatus',
         payload: response,
       }); // Login successfully
-      console.log("test12")
+      console.log("test1221212")
       if (response.code === 200) {
         const urlParams = new URL(window.location.href);
         const params = getPageQuery();
